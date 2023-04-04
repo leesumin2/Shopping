ef2b5e4b13b2f6d261f703a1da5ea8133341acd2
LEE SuMin <tnals2322@gmail.com>
2023-04-04 16:59:28 +0900

    JS : filtering

--- src/main.js
+++ src/main.js
@@ -6,11 +6,13 @@ function loadItems() {
         .then(json => json.items);
 }
 
+// Update the list with the given items
 function displayItems(items) {
     const container = document.querySelector('.items');
     container.innerHTML = items.map(item => createHTMLString(item)).join('');
 }
 
+// Cteate HTMl list item from the given
 function createHTMLString(item) {
     return `
     <li class="item">
@@ -20,11 +22,29 @@ function createHTMLString(item) {
     `;
 }
 
+// 필터링 
+function onButtonClick(event, items) {
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+
+    if (key == null || value == null) {
+        return;
+    }
+    displayItems(items.filter(item => item[key] === value));
+}
+
+function setEventListeners(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.clickbox');
+    logo.addEventListener('click', () => displayItems(items));
+    buttons.addEventListener('click', event => onButtonClick(event, items));
+}
 
 // main
 loadItems()
     .then(items => { // 받아 온 함수를 items를 함수에 전달
         displayItems(items);
-        // setEventListeners(items); // 받아 온 items를 이용하여 버튼 클릭 시 필터링하기 위해 적절한 이벤트리스너 작성
+        setEventListeners(items); // 받아 온 items를 이용하여 버튼 클릭 시 필터링하기 위해 적절한 이벤트리스너 작성
     })
     .catch(console.log);
