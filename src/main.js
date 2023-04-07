1a1dfb46b017453b5ba5911220012d5fe62af2e6
LEE SuMin <tnals2322@gmail.com>
2023-04-07 17:39:02 +0900

    JS : Visible

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ function loadItems() {
         .then(json => json.items);
 }
 
-// Update the list with the given items
+// Update the list with the given items(visible로 대체)
 function displayItems(items) {
     const container = document.querySelector('.items');
     container.innerHTML = items.map(item => createHTMLString(item)).join('');
@@ -22,16 +22,52 @@ function createHTMLString(item) {
     `;
 }
 
+function createElement(item) {
+    const img = document.createElement('img');
+    img.setAttribute('class', 'item__thumbnail');
+    img.setAttribute('src', item.image);
+    img.setAttribute('alt', item.type);
+
+    const span = document.createElement('span');
+    span.setAttribute('class', 'item__description');
+    span.innerText = `${item.gender}, ${item.size}`;
+
+    const li = document.createElement('li');
+    li.setAttribute('class', 'item');
+    li.setAttribute('data-type', item.type);
+    li.setAttribute('data-color', item.color);
+    li.append(img);
+    li.append(span);
+    return li;
+}
+
 // 필터링 
 function onButtonClick(event, items) {
     const dataset = event.target.dataset;
     const key = dataset.key;
     const value = dataset.value;
-
     if (key == null || value == null) {
         return;
     }
-    displayItems(items.filter(item => item[key] === value));
+
+    // displayItems(items.filter(item => item[key] === value));
+    visibleItems(items, key, value);
+}
+// 로고 버튼 눌렀을 때 전체 리스트
+function onLogoClick(event, items) {
+    items.forEach(c => {
+        c.classList.remove('invisible');
+    });
+}
+
+function visibleItems(items, key, value) {
+    items.forEach(item => {
+        if (item.dataset[key] === value) {
+            item.classList.remove('invisible');
+        } else {
+            item.classList.add('invisible');
+        }
+    });
 }
 
 function setEventListeners(items) {
@@ -44,7 +80,17 @@ function setEventListeners(items) {
 // main
 loadItems()
     .then(items => { // 받아 온 함수를 items를 함수에 전달
-        displayItems(items);
-        setEventListeners(items); // 받아 온 items를 이용하여 버튼 클릭 시 필터링하기 위해 적절한 이벤트리스너 작성
+        // displayItems(items);
+        // setEventListeners(items); 받아 온 items를 이용하여 버튼 클릭 시 필터링하기 위해 적절한 이벤트리스너 작성
+        const elements = items.map(createElement);
+        const container = document.querySelector('.items');
+        container.append(...elements);
+
+        const logo = document.querySelector('.logo');
+        logo.addEventListener('click', event => onLogoClick(event, elements));
+
+        const buttons = document.querySelector('.clickbox');
+        buttons.addEventListener('click', event => onButtonClick(event, elements));
+
     })
     .catch(console.log);
